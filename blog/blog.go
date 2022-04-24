684f4e650db3d81b1122a02a528887407b6f582f
André Eriksson <andre@encore.dev>
2022-04-24 18:10:36 +0200

    blog: fix nullable feature image

--- blog/blog.go
+++ blog/blog.go
@@ -2,6 +2,7 @@ package blog
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"time"
 
@@ -46,32 +47,36 @@ type GetBlogPostsResponse struct {
 // GetBlogPost retrieves a blog post by slug.
 //encore:api public method=GET path=/blog/:slug
 func GetBlogPost(ctx context.Context, slug string) (*BlogPost, error) {
-	// Use slug to query database...
-	var b BlogPost
+	var (
+		b   BlogPost
+		img sql.NullString
+	)
 	err := sqldb.QueryRow(ctx, `
 		SELECT slug, created_at, published, modified_at, title, summary, body, body_rendered, featured_image
 		FROM "article"
 		WHERE slug = $1
-	`, slug).Scan(&b.Slug, &b.CreatedAt, &b.Published, &b.ModifiedAt, &b.Title, &b.Summary, &b.Body, &b.BodyRendered, &b.FeaturedImage)
+	`, slug).Scan(&b.Slug, &b.CreatedAt, &b.Published, &b.ModifiedAt, &b.Title, &b.Summary, &b.Body, &b.BodyRendered, &img)
 	if err != nil {
 		return nil, &errs.Error{
 			Code:    errs.NotFound,
 			Message: "article not found",
 		}
 	}
+	b.FeaturedImage = img.String
 	return &b, nil
 }
 
 // CreateBlogPost creates a new blog post.
 //encore:api auth
 func CreateBlogPost(ctx context.Context, params *CreateBlogPostParams) error {
+	img := sql.NullString{String: params.FeaturedImage, Valid: params.FeaturedImage != ""}
 	rendered := blackfriday.Run([]byte(params.Body))
 	_, err := sqldb.Exec(ctx, `
 		INSERT INTO "article" (slug, created_at, published, modified_at, title, summary,body, body_rendered, featured_image)
 		VALUES ($1,  NOW(), $2, NOW(), $3, $4, $5, $6, $7)
 		ON CONFLICT (slug) DO UPDATE
 		SET published = $2, modified_at = NOW(), title = $3, summary = $4, body = $5, body_rendered = $6, featured_image = $7
-	`, params.Slug, params.Published, params.Title, params.Summary, params.Body, string(rendered), params.FeaturedImage)
+	`, params.Slug, params.Published, params.Title, params.Summary, params.Body, string(rendered), img)
 
 	if err != nil {
 		return fmt.Errorf("insert article: %v", err)
@@ -103,14 +108,18 @@ func GetBlogPosts(ctx context.Context, params *GetBlogPostsParams) (*GetBlogPost
 	var q []*BlogPost
 	var i = 0
 	for rows.Next() {
-		var b BlogPost
-		err := rows.Scan(&b.Slug, &b.CreatedAt, &b.Published, &b.ModifiedAt, &b.Title, &b.Summary, &b.Body, &b.BodyRendered, &b.FeaturedImage)
+		var (
+			b   BlogPost
+			img sql.NullString
+		)
+		err := rows.Scan(&b.Slug, &b.CreatedAt, &b.Published, &b.ModifiedAt, &b.Title, &b.Summary, &b.Body, &b.BodyRendered, &img)
 		if err != nil {
 			return &GetBlogPostsResponse{
 				Count:     0,
 				BlogPosts: []*BlogPost{},
 			}, err
 		}
+		b.FeaturedImage = img.String
 		q = append(q, &b)
 		i = i + 1
 	}
