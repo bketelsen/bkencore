7a0ae605648e2d692078f28150deeff50364daf1
Brian Ketelsen <bketelsen@gmail.com>
2022-05-16 12:22:33 -0400

    fix svg

--- frontend/components/LayoutDrawer.tsx
+++ frontend/components/LayoutDrawer.tsx
@@ -20,7 +20,7 @@ const Layout: FC<React.PropsWithChildren<{}>> = (props) => {
         <div className="max-w-5xl mx-auto navbar bg-base-300">
           <div className="flex-none lg:hidden">
             <label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 strokeCurrent"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
             </label>
           </div>
           <div className="flex-1 px-2 mx-2">
